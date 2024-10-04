c49951df72a7bb893071d5bc64e31ab2e2f1d7bc
luizTavolaro <luiz.felipetaa@gmail.com>
2024-10-04 15:41:01 -0300

    feat: toString

--- vis/hello.js
+++ vis/hello.js
@@ -139,7 +139,7 @@ looker.plugins.visualizations.add({
     // First pass: accumulate totals and store rows by product and canal
     data.forEach(function(row) {
       let produto = LookerCharts.Utils.htmlForCell(row[queryResponse.fields.dimensions[0].name]);
-      let logo = LookerCharts.Utils.htmlForCell(row[queryResponse.fields.dimensions[1].name]);
+      let codigoProduto = LookerCharts.Utils.htmlForCell(row[queryResponse.fields.dimensions[1].name]);
       let canal = LookerCharts.Utils.htmlForCell(row[queryResponse.fields.dimensions[2].name]);
       let edicao = LookerCharts.Utils.htmlForCell(row[queryResponse.fields.dimensions[3].name]);
       let sorteio = LookerCharts.Utils.htmlForCell(row[queryResponse.fields.dimensions[4].name]);
@@ -162,7 +162,7 @@ looker.plugins.visualizations.add({
           edicao: edicao,
           valor: valor,
           sorteio: sorteio,
-          logo: logo
+          codigoProduto: codigoProduto
         };
       }
 
@@ -200,7 +200,7 @@ looker.plugins.visualizations.add({
       let edicao = totalsByProduct[produto].edicao;
       let sorteio = totalsByProduct[produto].sorteio;
       let valor = totalsByProduct[produto].valor;
-      let logo = totalsByProduct[produto].logo;
+      let codigoProduto = totalsByProduct[produto].codigoProduto.toString();
 
       // Verificação de divisão por zero para total do produto
       var totalTitulosPromoAtualVar = (totalTitulosPromoAtual == 0 || totalTitulosPromoAnterior == 0)
@@ -223,7 +223,7 @@ looker.plugins.visualizations.add({
         <div class="resumo">
           <header>
             <div class="logo">
-                <img src=${logo} />
+                <img src="https://storage.googleapis.com/idea-data-homol-looker-artifacty/logo/${codigoProduto}.png" />
                 <p>${produto}</p>
             </div>
             <div>
