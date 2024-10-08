353d080cfa10c797dff472643075b581bdccc5d2
luizTavolaro <luiz.felipetaa@gmail.com>
2024-10-08 11:42:37 -0300

    feat: paginas

---
+++ vis/paginas.js
@@ -0,0 +1,54 @@
+looker.plugins.visualizations.add({
+    options: {
+      font_size: {
+        type: "string",
+        label: "Font Size",
+        values: [
+          {"Large": "large"},
+          {"Small": "small"}
+        ],
+        display: "radio",
+        default: "large"
+      }
+    },
+  
+    create: function(element, config) {
+      element.innerHTML = `
+        <style>
+        </style>
+      `;
+  
+      var container = element.appendChild(document.createElement("div"));
+      this._tableElement = container.appendChild(document.createElement("div"));
+    },
+  
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+      this.clearErrors();
+  
+      if (queryResponse.fields.dimensions.length == 0 && queryResponse.fields.measures.length == 0) {
+        this.addError({title: "No Data", message: "This chart requires dimensions or measures."});
+        return;
+      }
+  
+      let paginas;
+  
+      data.forEach(function(row) {
+        paginas = LookerCharts.Utils.htmlForCell(row[queryResponse.fields.dimensions[0].name]);
+      });
+      
+      let htmlContent = '';
+
+      paginas.forEach(function(pg) {
+        htmlContent += `<p>${pg}</p>`;
+      });
+      
+  
+      // Insert the generated HTML into the container
+      this._tableElement.innerHTML = htmlContent;
+  
+      done();
+    }
+  
+  
+  });
+  
